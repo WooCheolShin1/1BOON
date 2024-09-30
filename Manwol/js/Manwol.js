17c37816d8b9364a22406f5768a826c46ffefdbe
WooCheolShin <sigon1112@naver.com>
2024-09-30 19:02:04 +0900

    first commit

--- Manwol/js/Manwol.js
+++ Manwol/js/Manwol.js
@@ -39,3 +39,91 @@
       }, 100);
     }
 
+    const ani1 = gsap.timeline({
+      defaults:{
+        autoAlpha:0,
+      },
+      ease: "none",
+    });
+
+    const ani2 = gsap.timeline({
+      defaults:{
+        autoAlpha:0,
+      },
+      ease: "none"
+    });
+
+    const ani3 = gsap.timeline({
+      defaults:{
+        autoAlpha:0,
+      },
+      ease: "none"
+    });
+
+    const ani4 = gsap.timeline({});
+    const ani5 = gsap.timeline({});
+
+    ani1.from("#scrollmanwol",{
+      x: -100,
+    })
+
+    ani2.from("#scrollmanwol2", {
+
+    })
+    
+    ani3.from("#scrollmanwol3", {
+      y: 100
+    })
+
+    ani4.from("#scrollmanwol4",{
+      x: -50,
+    })
+    
+    ani5.from("#scrollmanwol5", {
+      x: 50
+    })
+    
+    
+    ScrollTrigger.create({
+      animation: ani1,
+      trigger:"#scrollmanwol",
+      start:"-=500",
+      end:"+=0",
+      scrub: 5,
+      anticipatePin: 1,
+	  })
+
+    ScrollTrigger.create({
+      animation: ani2,
+      trigger:"#scrollmanwol2",
+      start:"-=600",
+      end:"+=0",
+      scrub: 5,
+      anticipatePin: 1,
+      opacity: 1,
+	  })
+
+    ScrollTrigger.create({
+      animation: ani3,
+      trigger:"#scrollmanwol3",
+      start:"-=500",
+      end:"+=0",
+      scrub: 5,
+      anticipatePin: 1,
+	  })
+
+    ScrollTrigger.create({
+      animation: ani4,
+      trigger:"#scrollmanwol4",
+      start:"-=800",
+      end:"+=0",
+      scrub: 5,
+	  })
+
+    ScrollTrigger.create({
+      animation: ani5,
+      trigger:"#scrollmanwol5",
+      start:"-=800",
+      end:"+=0",
+      scrub: 5,
+	  })
