7ddc0e91bef05af1325cbd2e7be54eea834afce0
ShinWoocheol <sigon1112@naver.com>
2024-10-14 01:36:15 +0900

    first commit

--- landingpage/js/scripts.js
+++ landingpage/js/scripts.js
@@ -1,7 +1,24 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+$(".ottogi ul li:first-child").hover(function(e){
+    e.preventDefault();
+    $(".project_explain .project_explain1 p .span1").css("display", "block");
+  })
+
+$(".ottogi ul li:nth-child(2)").hover(function(e){
+    e.preventDefault();
+    $(".project_explain .project_explain1 p .span2").css("display", "block");
+  })
+
+$(".ottogi ul li:nth-child(3)").hover(function(e){
+    e.preventDefault();
+    $(".project_explain .project_explain1 p .span3").css("display", "block");
+  })
+
+$(".ottogi ul li:nth-child(4)").hover(function(e){
+    e.preventDefault();
+    $(".project_explain .project_explain1 p .span4").css("display", "block");
+  })
+
+  $(".ottogi ul li").mouseleave(function(e){
+    e.preventDefault();
+    $(".project_explain .project_explain1 p span").css("display", "none");
+  })
